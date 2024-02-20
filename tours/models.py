a11be50ff8a5cfa8f15448152396fefca0cbd738
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-20 14:30:03 +0600

    Define TourCategory, Tours, and Recommendation models

--- tours/models.py
+++ tours/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class TourCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Tours(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    address = models.CharField(max_length=255)
+    photo = models.ImageField()
+    description = models.TextField()
+    is_active = models.BooleanField(default=False)
+    best_for_season = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Recommendation(models.Model):
+    current_time = models.DateField(auto_add_now=True)
+    tours = models.OneToOneField(Tours, on_delete=models.CASCADE)
+
