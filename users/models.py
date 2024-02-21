6b40edb79deaf032bc1d9f498b6fb4edf748ee4a
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-21 18:22:10 +0600

    Customize a User model

--- users/models.py
+++ users/models.py
@@ -3,6 +3,20 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    phone_number = PhoneNumberField()
+    is_active = models.BooleanField(default=True)
 
+    USERNAME_FIELD = "username"
 
+    def __str__(self):
+        return f"{self.username} - {self.first_name}"
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
