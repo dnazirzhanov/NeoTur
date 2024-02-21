e893d337690b505cf9402bbaffbf81cb932343aa
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-21 16:34:47 +0600

    Create a PostgreSQL database for this project

--- neotur/settings.py
+++ neotur/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'neotur.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_for_neotur',
+        'USER': 'postgres',
+        'PASSWORD': 'dastanforneobis',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
