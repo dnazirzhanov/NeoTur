2bb486240ac8f7fa2e2f734c6d971e3045d1bc2a
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-22 15:40:11 +0600

    create a 'Order' model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from users.models import User
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total_price = models.IntegerField()
+
+
+
