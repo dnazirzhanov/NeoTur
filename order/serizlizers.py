9298293d8501740613ac2737b86a582d7111e033
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-22 16:09:30 +0600

    create a function to calculate a total price of users' orders

--- order/serizlizers.py
+++ order/serizlizers.py
@@ -2,12 +2,18 @@ from .models import Order
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
+from tours.models import Tour
 
 class OrderSerializer(ModelSerializer):
     total_price = SerializerMethodField('get_total_price')
 
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = ["user", "quantity"]
 
-
+        def get_total_price(self, obj):
+            tours = Tours.objects.filter(order=obj.id)
+            total_price = 0
+            for tour in tours:
+                total_price += tour.price
+            return total_price
