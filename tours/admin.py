99604ba8b0087c30329260415e175e5cca695648
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-22 17:09:55 +0600

    registering all the models into the admin panel

--- tours/admin.py
+++ tours/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tours, TourCategory, Recommendation
 
-# Register your models here.
+admin.site.register(Tours)
+admin.site.register(TourCategory)
+admin.site.register(Recommendation)
