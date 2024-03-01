75c1361c1a7af203da4a41909eff6982a3c2188c
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-03-01 15:44:44 +0600

    Fix a small bug

--- tours/serializers.py
+++ tours/serializers.py
@@ -1,23 +1,23 @@
-from .models import Recommendation, Tours, TourCategory
+from .models import Tours, TourCategory, Recommendation
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 class ToursSerializer(ModelSerializer):
     class Meta:
         model = Tours
-        fields = ["__all__"]
+        fields = "__all__"
 
 
 class TourCategorySerializer(ModelSerializer):
     class Meta:
         model = TourCategory
-        fields = ["__all__"]
+        fields = "__all__"
 
 
 class RecommendationSerializer(ModelSerializer):
     class Meta:
         model = Recommendation
-        fields = ["__all__"]
+        fields = "__all__"
 
 
 
