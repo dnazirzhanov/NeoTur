410d9f4c92b8d4549e206cb35c6f6e9ed57bd855
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-03-01 15:17:31 +0600

    Add a 'api_schema' url to view the list of urls and others

--- neotur/urls.py
+++ neotur/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
+    path('api_schema/', get_schema_view(
+        title='API Schema',
+        description='Guide for the REST API'
+    ), name='api_schema'),
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('order.urls')),
